08247fc0cab92aea190190d252f35c761fc68660
APCTophat <49924900+APCTophat@users.noreply.github.com>
2020-03-03 09:50:54 +0200

    The button does a thing

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
-let c = 1.01 + 2.02;
-//c = 3.03
-console.log(c);
 
-function wat(thingy){
-    console.log('wat'+ thingy);
 
-}
-
-wat(2);
-
-const button = document.children[0];
-console.log(button);
+const button = document.querySelector('button');
+const article = document.querySelector('article');
 
 button.addEventListener('click', function(){
     const el = document.createElement('em');
-    el.innerText('Something');
-    document.body.appendChild(el);
+    el.innerText = 'something';
+    article.prepend(el);
 })
