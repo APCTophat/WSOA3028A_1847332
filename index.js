646317e4da1f2e191e3b879d0a298a38cfe5bbeb
APCTophat <49924900+APCTophat@users.noreply.github.com>
2020-03-03 09:18:08 +0200

    buton should od something

--- index.js
+++ index.js
@@ -11,3 +11,9 @@ wat(2);
 
 const button = document.body.children[1];
 console.log(button);
+
+button.addEventListener('click', function(e){
+    const el = document.createElement('em');
+    el.innerText('Something');
+    document.body.appendChild(el);
+})
