cc59c348ed15fb2b7dc6492faebbf16bb6d26419
APCTophat <49924900+APCTophat@users.noreply.github.com>
2020-03-18 19:01:29 +0200

    Moved blogs into their own pages

--- development-blogs/blog-archive.js
+++ development-blogs/blog-archive.js
@@ -1,23 +1,23 @@
 let backToHomeButton = document.getElementsByClassName("back_to_homepage")[0];
 backToHomeButton.addEventListener('click',function(){location.href = "../index.html";});
 
-let blogDesignResearch = document.getElementsByClassName("to_development_blog")[1];
-blogTheGreatReformat.addEventListener('click', function(){location.href="blog-archive/blog-2020-02-03/blog-design-research.html";});
+let blogDesignResearch = document.getElementsByClassName("to_development_blog")[0];
+blogDesignResearch.addEventListener('click', function(){location.href="blog-archive/blog-2020-02-03/blog-design-research.html";});
 
-let blogCharacterDesign = document.getElementsByClassName("to_development_blog")[2];
-blogTheGreatReformat.addEventListener('click', function(){location.href="blog-archive/blog-2020-02-03/blog-design-research.html";});
+let blogCharacterDesign = document.getElementsByClassName("to_development_blog")[1];
+blogCharacterDesign.addEventListener('click', function(){location.href="blog-archive/blog-2020-02-10/blog-character-design.html";});
 
-let blogHyperlinking = document.getElementsByClassName("to_development_blog")[3];
-blogTheGreatReformat.addEventListener('click', function(){location.href="blog-archive/blog-2020-02-03/blog-design-research.html";});
+let blogHyperlinking = document.getElementsByClassName("to_development_blog")[2];
+blogHyperlinking.addEventListener('click', function(){location.href="blog-archive/blog-2020-02-17/blog-hyperlinking.html";});
 
-let blogMediumMessage = document.getElementsByClassName("to_development_blog")[4];
-blogTheGreatReformat.addEventListener('click', function(){location.href="blog-archive/blog-2020-02-03/blog-design-research.html";});
+let blogMediumMessage = document.getElementsByClassName("to_development_blog")[3];
+blogMediumMessage.addEventListener('click', function(){location.href="blog-archive/blog-2020-02-24/blog-medium-is-the-message.html";});
 
-let blogMetadata = document.getElementsByClassName("to_development_blog")[5];
-blogTheGreatReformat.addEventListener('click', function(){location.href="blog-archive/blog-2020-02-03/blog-design-research.html";});
+let blogMetadata = document.getElementsByClassName("to_development_blog")[4];
+blogMetadata.addEventListener('click', function(){location.href="blog-archive/blog-2020-03-02/blog-metadata.html";});
 
-let blogMicroformats = document.getElementsByClassName("to_development_blog")[6];
-blogTheGreatReformat.addEventListener('click', function(){location.href="blog-archive/blog-2020-02-03/blog-design-research.html";});
+let blogMicroformats = document.getElementsByClassName("to_development_blog")[5];
+blogMicroformats.addEventListener('click', function(){location.href="blog-archive/blog-2020-03-09/blog-microformats.html";});
 
-let blogTheGreatReformat = document.getElementsByClassName("to_development_blog")[7];
+let blogTheGreatReformat = document.getElementsByClassName("to_development_blog")[6];
 blogTheGreatReformat.addEventListener('click', function(){location.href="blog-archive/blog-2020-03-16/blog-the-great-reformat.html";});
