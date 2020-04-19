7ef24de3a93d771381b212ce6a44b6531730948c
APCTophat <49924900+APCTophat@users.noreply.github.com>
2020-04-19 18:33:28 +0200

    Got the basic navigation between the components ready

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 let blogArchiveButton = document.getElementsByClassName("blog_archive_button")[0];
 blogArchiveButton.addEventListener('click', function(){location.href = "development-blogs/blog-archive.html";});
 
+let wireframeButton = document.getElementsByClassName("wireframe_button")[0];
+wireframeButton.addEventListener('click', function(){location.href = "website-wireframe/website-wireframe.html";});
+
+let websiteStyleButton = document.getElementsByClassName("website_style")[0];
+websiteStyleButton.addEventListener('click',function(){location.href = "website-style/website-style.html";});
+
+let characterDesignButton = document.getElementsByClassName("character_design")[0];
+characterDesignButton.addEventListener('click',function(){location.href = "website-character-design/website-character-design.html";});
+
 let postArchiveButton = document.getElementsByClassName("post_archive_button")[0];
 postArchiveButton.addEventListener('click', function(){location.href = "dennis-scotts's-posts/post-archive.html";});
+
+let dennisScottAboutButton = document.getElementsByClassName("dennis_scott_about")[0];
+dennisScottAboutButton.addEventListener('click',function(){location.href = "dennis-scott-about/dennis-scott-about.html";});
+
+let dennisScottContactButton = document.getElementsByClassName("dennis_scott_contact")[0];
+dennisScottContactButton.addEventListener('click',function(){location.href = "dennis-scott-contact-information/dennis-scott-contact.html";});
---
+++ website-style/website-style.js
@@ -0,0 +1,2 @@
+let backToHomeButton = document.getElementsByClassName("back_to_homepage")[0];
+backToHomeButton.addEventListener('click',function(){location.href = "../index.html";});
