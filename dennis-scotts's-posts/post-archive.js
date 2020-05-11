343ab3fa5986d1b08ef39f70cf4de1272af47313
APCTophat <49924900+APCTophat@users.noreply.github.com>
2020-05-11 16:03:46 +0200

    I AM DOING THE THINGS
    It is starting to look like an actual website hey.

--- dennis-scotts's-posts/post-archive.js
+++ dennis-scotts's-posts/post-archive.js
@@ -12,3 +12,6 @@ post2020April09.addEventListener('click', function(){location.href = "post-archi
 
 let post2020April21 = document.getElementsByClassName("to_scotts_posts")[3];
 post2020April21.addEventListener('click', function(){location.href = "post-archive/post-2020-04-21/post-2020-04-21.html"})
+
+let post2020May08 = document.getElementsByClassName("to_scotts_posts")[4];
+post2020May08.addEventListener('click', function(){location.href = "post-archive/post-2020-05-08/post-2020-05-08.html"})
