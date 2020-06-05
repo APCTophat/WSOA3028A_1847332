9a85e3751c8b2170c31346fa3e5c6ff2de938054
APCTophat <49924900+APCTophat@users.noreply.github.com>
2020-06-05 11:56:59 +0200

    05/06/2020 Assignment 3 submission

--- dennis-scotts's-posts/post-archive/back-to-post-archive.js
+++ dennis-scotts's-posts/post-archive/back-to-post-archive.js
@@ -1,2 +1,3 @@
 let backToHomeButton = document.getElementsByClassName("back_to_post_archive")[0];
 backToHomeButton.addEventListener('click',function(){location.href = "../../post-archive.html";});
+backToHomeButton.style.cursor = "pointer";
--- development-blogs/blog-archive/back-to-archive.js
+++ development-blogs/blog-archive/back-to-archive.js
@@ -1,2 +1,3 @@
 let backToHomeButton = document.getElementsByClassName("back_to_blog_archive")[0];
 backToHomeButton.addEventListener('click',function(){location.href = "../../blog-archive.html";});
+backToHomeButton.style.cursor = "pointer";
