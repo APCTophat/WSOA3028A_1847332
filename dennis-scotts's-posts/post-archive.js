d5a67723a75a007ee1f7f36bd10cb33097b7f68d
APCTophat <49924900+APCTophat@users.noreply.github.com>
2020-06-05 11:19:07 +0200

    Added Pointer

--- dennis-scotts's-posts/post-archive.js
+++ dennis-scotts's-posts/post-archive.js
@@ -1,27 +1,36 @@
 
 let post2020March26 = document.getElementsByClassName("to_scotts_posts")[0];
 post2020March26.addEventListener('click', function(){location.href = "post-archive/post-2020-03-26/post-2020-03-26.html"})
+post2020March26.style.cursor = "pointer";
 
 let post2020April02 = document.getElementsByClassName("to_scotts_posts")[1];
 post2020April02.addEventListener('click', function(){location.href = "post-archive/post-2020-04-02/post-2020-04-02.html"})
+post2020April02.style.cursor = "pointer";
 
 let post2020April09 = document.getElementsByClassName("to_scotts_posts")[2];
 post2020April09.addEventListener('click', function(){location.href = "post-archive/post-2020-04-09/post-2020-04-09.html"})
+post2020April09.style.cursor = "pointer";
 
 let post2020April21 = document.getElementsByClassName("to_scotts_posts")[3];
 post2020April21.addEventListener('click', function(){location.href = "post-archive/post-2020-04-21/post-2020-04-21.html"})
+post2020April21.style.cursor = "pointer";
 
 let post2020May08 = document.getElementsByClassName("to_scotts_posts")[4];
 post2020May08.addEventListener('click', function(){location.href = "post-archive/post-2020-05-08/post-2020-05-08.html"})
+post2020May08.style.cursor = "pointer";
 
 let post2020May23 = document.getElementsByClassName("to_scotts_posts")[5];
 post2020May23.addEventListener('click', function(){location.href = "post-archive/post-2020-05-23/post-2020-05-23.html"})
+post2020May23.style.cursor = "pointer";
 
 let post2020May30 = document.getElementsByClassName("to_scotts_posts")[6];
 post2020May30.addEventListener('click', function(){location.href = "post-archive/post-2020-05-30/post-2020-05-30.html"})
+post2020May30.style.cursor = "pointer";
 
 let post2020June1 = document.getElementsByClassName("to_scotts_posts")[7];
 post2020June1.addEventListener('click', function(){location.href = "post-archive/post-2020-06-01/post-2020-06-01.html"})
+post2020June1.style.cursor = "pointer";
 
 let post2020June2 = document.getElementsByClassName("to_scotts_posts")[8];
 post2020June2.addEventListener('click', function(){location.href = "post-archive/post-2020-06-02/post-2020-06-02.html"})
+post2020June2.style.cursor = "pointer";
--- development-blogs/blog-archive.js
+++ development-blogs/blog-archive.js
@@ -1,38 +1,51 @@
 let blogWebsiteExplination = document.getElementsByClassName("to_development_blog")[0];
 blogWebsiteExplination.addEventListener('click', function(){location.href="blog-archive/blog-2020-02-01/blog-website-explination.html";});
+blogWebsiteExplination.style.cursor = "pointer";
 
 let blogDesignResearch = document.getElementsByClassName("to_development_blog")[1];
 blogDesignResearch.addEventListener('click', function(){location.href="blog-archive/blog-2020-02-03/blog-design-research.html";});
+blogDesignResearch.style.cursor = "pointer";
 
 let blogCharacterDesign = document.getElementsByClassName("to_development_blog")[2];
 blogCharacterDesign.addEventListener('click', function(){location.href="blog-archive/blog-2020-02-10/blog-character-design.html";});
+blogCharacterDesign.style.cursor = "pointer";
 
 let blogHyperlinking = document.getElementsByClassName("to_development_blog")[3];
 blogHyperlinking.addEventListener('click', function(){location.href="blog-archive/blog-2020-02-17/blog-hyperlinking.html";});
+blogHyperlinking.style.cursor = "pointer";
 
 let blogMediumMessage = document.getElementsByClassName("to_development_blog")[4];
 blogMediumMessage.addEventListener('click', function(){location.href="blog-archive/blog-2020-02-24/blog-medium-is-the-message.html";});
+blogMediumMessage.style.cursor = "pointer";
 
 let blogMetadata = document.getElementsByClassName("to_development_blog")[5];
 blogMetadata.addEventListener('click', function(){location.href="blog-archive/blog-2020-03-02/blog-metadata.html";});
+blogMetadata.style.cursor = "pointer";
 
 let blogMicroformats = document.getElementsByClassName("to_development_blog")[6];
 blogMicroformats.addEventListener('click', function(){location.href="blog-archive/blog-2020-03-09/blog-microformats.html";});
+blogMicroformats.style.cursor = "pointer";
 
 let blogTheGreatReformat = document.getElementsByClassName("to_development_blog")[7];
 blogTheGreatReformat.addEventListener('click', function(){location.href="blog-archive/blog-2020-03-16/blog-the-great-reformat.html";});
+blogTheGreatReformat.style.cursor = "pointer";
 
 let blogStockPhoto = document.getElementsByClassName("to_development_blog")[8];
 blogStockPhoto.addEventListener('click', function(){location.href="blog-archive/blog-2020-04-06/blog-stock-photos.html";});
+blogStockPhoto.style.cursor = "pointer";
 
 let blogInfoGeo = document.getElementsByClassName("to_development_blog")[9];
 blogInfoGeo.addEventListener('click', function(){location.href="blog-archive/blog-2020-04-20/blog-informational-geography.html";});
+blogInfoGeo.style.cursor = "pointer";
 
 let blogWebsiteAnalysis = document.getElementsByClassName("to_development_blog")[10];
 blogWebsiteAnalysis.addEventListener('click', function(){location.href="blog-archive/blog-2020-04-28/blog-website-analysis.html";});
+blogWebsiteAnalysis.style.cursor = "pointer";
 
-let SMI = document.getElementsByClassName("to_development_blog")[11];
-SMI.addEventListener('click', function(){location.href="blog-archive/blog-2020-06-01/blog-SMI.html";});
+let blogSMI = document.getElementsByClassName("to_development_blog")[11];
+blogSMI.addEventListener('click', function(){location.href="blog-archive/blog-2020-06-01/blog-SMI.html";});
+blogSMI.style.cursor = "pointer";
 
-let TechPower = document.getElementsByClassName("to_development_blog")[12];
-TechPower.addEventListener('click', function(){location.href="blog-archive/blog-2020-06-03/blog-technology-power-relationship.html";});
+let blogTechPower = document.getElementsByClassName("to_development_blog")[12];
+blogTechPower.addEventListener('click', function(){location.href="blog-archive/blog-2020-06-03/blog-technology-power-relationship.html";});
+blogTechPower.style.cursor = "pointer";
